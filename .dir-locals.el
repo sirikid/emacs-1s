a9f68301eb85e26ec149519cccdb6a2052db34be
Ivan Sokolov <ivan-p-sokolov@ya.ru>
2021-06-14 19:41:37 +0300

    Add .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((compile-command . "eldev build")))
+ (emacs-lisp-mode . ((indent-tabs-mode . nil))))
